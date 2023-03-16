9558511a6699315d2a9401b088f15a970f30fde8
AYLA71 <ayla44aya@gmail.com>
2023-03-16 13:29:34 -0700

    tash4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /*
  * main - this main fonction
  *
- * Return: always 0 (success)
+ * Return: 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
