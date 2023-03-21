5e150167cb17ec1c54a00919dc12eab385b00eb9
AYLA71 <ayla44aya@gmail.com>
2023-03-21 13:21:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - this is main fonction
+ * main - Entry point main function
  *
- *Return: 0 always
+ * Return:  Always 0
  */
+
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
